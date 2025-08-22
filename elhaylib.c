801e5a2d036e0453f9c6630ac04efa07b58d1fc0
Karim El-Hayawan <snackage@gmail.com>
2025-08-23 01:39:37 +0200

    first linked list implementations

--- elhaylib.c
+++ elhaylib.c
@@ -6,7 +6,7 @@
 #include <string.h>
 
 // DYNAMIC ARRAY
-void dynarr_init(dynarr_head* ptr_head) {
+void dynarr_init(dynarr_head* const ptr_head) {
     assert(ptr_head->elem_size > 0 && "Element size must be greater zero.");
     if(ptr_head->dynarr_capacity <= 0) {
         ptr_head->dynarr_capacity = 10;
@@ -21,7 +21,7 @@ void dynarr_init(dynarr_head* ptr_head) {
         calloc(ptr_head->dynarr_capacity, ptr_head->elem_size);
 }
 
-void dynarr_push(dynarr_head* ptr_head, const void* element) {
+void dynarr_push(dynarr_head* const ptr_head, const void* element) {
     if(ptr_head->dynarr_size == ptr_head->dynarr_capacity) {
         dynarr_expand(ptr_head);
     }
@@ -32,14 +32,14 @@ void dynarr_push(dynarr_head* ptr_head, const void* element) {
     ++ptr_head->dynarr_size;
 }
 
-void dynarr_free(dynarr_head* ptr_head) {
+void dynarr_free(dynarr_head* const ptr_head) {
     free(ptr_head->ptr_first_elem);
     ptr_head->dynarr_size = 0;
     ptr_head->ptr_first_elem = NULL;
 }
 
 // internals
-void dynarr_expand(dynarr_head* ptr_head) {
+void dynarr_expand(dynarr_head* const ptr_head) {
     ptr_head->dynarr_capacity =
         (size_t)(ptr_head->dynarr_capacity * ptr_head->growth_fac);
     void* new_ptr = realloc(ptr_head->ptr_first_elem,
@@ -52,3 +52,48 @@ void dynarr_expand(dynarr_head* ptr_head) {
 }
 
 // LINKED LIST
+void linlst_init(linked_list_head* const ptr_head) {
+    assert(ptr_head->list_type > 0 && "List-type must be defined");
+    ptr_head->list_len = 0;
+    ptr_head->ptr_first_node = NULL;
+    ptr_head->ptr_last_node = NULL;
+}
+
+void linlist_append_node(linked_list_head* const ptr_head,
+                         size_t data_size,
+                         const void const* data) {
+    list_node* new_node_ptr = calloc(1, sizeof(list_node));
+    new_node_ptr->data = calloc(1, data_size);
+    memcpy(new_node_ptr->data, data, data_size);
+
+    ptr_head->ptr_last_node = new_node_ptr;
+    if(ptr_head->list_len == 0) {
+        ptr_head->list_len++;
+        ptr_head->ptr_first_node = new_node_ptr;
+
+        switch(ptr_head->list_type) {
+            case CIRCULAR:
+                new_node_ptr->next_node = new_node_ptr;
+                new_node_ptr->previous_node = new_node_ptr;
+                break;
+            case OPEN:
+                break;
+        }
+        return;
+    }
+
+    list_node* old_last_node_ptr = ptr_head->ptr_last_node;
+    old_last_node_ptr->next_node = new_node_ptr;
+    new_node_ptr->previous_node = old_last_node_ptr;
+
+    switch(ptr_head->list_type) {
+        case CIRCULAR:
+            new_node_ptr->next_node = ptr_head->ptr_first_node;
+            ptr_head->ptr_last_node = new_node_ptr;
+            break;
+        case OPEN:
+            break;
+    }
+
+    ptr_head->list_len++;
+}
--- elhaylib.h
+++ elhaylib.h
@@ -1,4 +1,5 @@
 #include <inttypes.h>
+#include <stdbool.h>
 #include <stddef.h>
 
 typedef enum { OK } ERROR_CODES;
@@ -12,24 +13,20 @@ typedef struct {
     char* ptr_first_elem;
 } dynarr_head;
 
-void dynarr_init(dynarr_head* ptr_head);
-void dynarr_push(dynarr_head* ptr_head, const void* element);
-void dynarr_free(dynarr_head* ptr_head);
+void dynarr_init(dynarr_head* const ptr_head);
+void dynarr_push(dynarr_head* const ptr_head, const void const* element);
+void dynarr_free(dynarr_head* const ptr_head);
 // internals
-void dynarr_expand(dynarr_head* ptr_head);
+void dynarr_expand(dynarr_head* const ptr_head);
 
 // LINKED LIST
-typedef enum {
-    SINGLE_OPEN,
-    SINGLE_CIRCULAR,
-    DOUBLE_OPEN,
-    DOUBLE_CIRCULAR,
-} list_type;
+typedef enum { OPEN = 1, CIRCULAR } list_type;
+
 typedef struct {
     list_type list_type;
-    size_t elem_size;
     size_t list_len;
-    char* ptr_first_elem;
+    char* ptr_first_node;
+    char* ptr_last_node;
 } linked_list_head;
 
 typedef struct {
@@ -37,3 +34,19 @@ typedef struct {
     struct list_node* next_node;
     struct list_node* previous_node;
 } list_node;
+
+void linlst_init(linked_list_head* const ptr_head);
+void linlist_append_node(linked_list_head* const ptr_head,
+                         size_t data_size,
+                         const void const* data);
+void linlist_insert_node(linked_list_head* const ptr_head,
+                         list_node* const pre_node,
+                         size_t data_size,
+                         const void const* data);
+void linlist_delete_node(linked_list_head* const ptr_head,
+                         list_node* const node);
+void linlist_delete_list(linked_list_head* const ptr_head);
+bool linlist_get_node(linked_list_head* const ptr_head,
+                      list_node* found_node,
+                      int index);
+bool linlist_is_empty(linked_list_head* const ptr_head);
