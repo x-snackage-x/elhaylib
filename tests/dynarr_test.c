42038cb7ede49ee89128e6d90d4add1e0175d31e
Karim El-Hayawan <snackage@gmail.com>
2025-09-08 17:19:01 +0200

    fix dynamic array tests

--- tests/dynarr_test.c
+++ tests/dynarr_test.c
@@ -20,10 +20,22 @@ typedef struct {
     void* aPointer;
 } my_test_struct;
 
+void print_test_res(bool result) {
+    if(result) {
+        printf(GRN "S" RESET);
+    } else {
+        printf(RED "F" RESET);
+    }
+    printf("\n");
+}
+
 bool test_primitive_types(bool no_yapping) {
     bool test_result = true;
+    int n_sub = 3;
+    bool sub_tests[] = {true, true, true};
+
     if(!no_yapping)
-        printf("1. Testing dynamic array with primitive types:\n");
+        printf("1. Testing dynamic array with scaler types:\n");
 
     dynarr_head my_arr = {
         .elem_size = sizeof(float), .dynarr_capacity = 3, .growth_fac = 1.5f};
@@ -31,7 +43,7 @@ bool test_primitive_types(bool no_yapping) {
 
     float* ptr_head_float = (float*)my_arr.ptr_first_elem;
 
-    float my_floats[] = {1.5437f, 25.246f, 333.33f, 4926.3f, 54535.f};
+    float my_floats[] = {1.5437f, 25.246f, 333.35f, 4926.3f, 54535.f};
 
     *(ptr_head_float) = my_floats[0];
     *(ptr_head_float + 1) = my_floats[1];
@@ -39,19 +51,20 @@ bool test_primitive_types(bool no_yapping) {
     my_arr.dynarr_size = 3;
 
     for(long unsigned int i = 0; i < my_arr.dynarr_size; ++i) {
-        test_result &= (my_floats[i] == *(ptr_head_float + i));
+        sub_tests[0] &= (my_floats[i] == *(ptr_head_float + i));
         if(!no_yapping)
-            printf("%ld-th Element = %f\n", i, *(ptr_head_float + i) + 1);
+            printf("%ld-th Element = %f\n", i, *(ptr_head_float + i));
     }
 
     if(!no_yapping) {
+        printf("%*s", TEST_NAMES_LENGTH, "Result: ");
+        print_test_res(sub_tests[0]);
         printf("Testing expansion:\n");
         printf("Before Size: %zu\n", my_arr.dynarr_size);
         printf("Before Cap.: %zu\n", my_arr.dynarr_capacity);
     }
 
-    my_floats[3] = 4926.3f;
-    dynarr_push(&my_arr, &my_floats[3]);
+    ptr_head_float = (float*)dynarr_push(&my_arr, &my_floats[3]);
     if(!no_yapping) {
         printf("After Size: %zu\n", my_arr.dynarr_size);
         printf("After Cap.: %zu\n", my_arr.dynarr_capacity);
@@ -62,27 +75,39 @@ bool test_primitive_types(bool no_yapping) {
             printf("%ld-th Element = %f\n", i, *(ptr_head_float + i));
     }
 
-    my_floats[4] = 54535.f;
-    dynarr_push(&my_arr, &my_floats[4]);
+    ptr_head_float = (float*)dynarr_push(&my_arr, &my_floats[4]);
     if(!no_yapping) {
+        printf("%*s", TEST_NAMES_LENGTH, "Result: ");
+        print_test_res(sub_tests[1]);
         printf("After Size: %zu\n", my_arr.dynarr_size);
         printf("After Cap.: %zu\n", my_arr.dynarr_capacity);
     }
     for(long unsigned int i = 0; i < my_arr.dynarr_size; ++i) {
-        test_result &= (my_floats[i] == *(ptr_head_float + i));
+        sub_tests[2] &= (my_floats[i] == *(ptr_head_float + i));
         if(!no_yapping)
             printf("%ld-th Element = %f\n", i, *(ptr_head_float + i));
+    }
+    if(!no_yapping) {
+        printf("%*s", TEST_NAMES_LENGTH, "Result: ");
+        print_test_res(sub_tests[2]);
     }
 
     dynarr_free(&my_arr);
 
+    for(int i = 0; i < n_sub; ++i) {
+        test_result &= sub_tests[i];
+    }
     return test_result;
 }
 
 bool test_struct(bool no_yapping) {
     bool test_result = true;
+    int n_sub = 3;
+    bool sub_tests[] = {true, true, true};
+
     if(!no_yapping)
         printf("\n2. Testing dynamic array with a struct:\n");
+
     dynarr_head my_arr = {0};
     my_arr.elem_size = sizeof(my_test_struct);
     my_arr.dynarr_capacity = 2;
@@ -111,16 +136,16 @@ bool test_struct(bool no_yapping) {
     my_structs[2].aPointer = &my_structs[1];
     my_structs[3].aPointer = &my_structs[2];
 
-    dynarr_push(&my_arr, &my_structs[0]);
-    dynarr_push(&my_arr, &my_structs[1]);
+    ptr_head = (my_test_struct*)dynarr_push(&my_arr, &my_structs[0]);
+    ptr_head = (my_test_struct*)dynarr_push(&my_arr, &my_structs[1]);
 
     my_test_struct* ptr_data = 0;
     for(long unsigned int i = 0; i < my_arr.dynarr_size; ++i) {
         ptr_data = (ptr_head + i);
-        test_result &= (my_structs[i].anInt == ptr_data->anInt);
-        test_result &= (my_structs[i].aFloat == ptr_data->aFloat);
-        test_result &= (my_structs[i].aDouble == ptr_data->aDouble);
-        test_result &= (my_structs[i].aPointer == ptr_data->aPointer);
+        sub_tests[0] &= (my_structs[i].anInt == ptr_data->anInt);
+        sub_tests[0] &= (my_structs[i].aFloat == ptr_data->aFloat);
+        sub_tests[0] &= (my_structs[i].aDouble == ptr_data->aDouble);
+        sub_tests[0] &= (my_structs[i].aPointer == ptr_data->aPointer);
 
         if(!no_yapping) {
             printf("%ld-th Struct:\n", i);
@@ -134,16 +159,18 @@ bool test_struct(bool no_yapping) {
     }
 
     if(!no_yapping) {
+        printf("%*s", TEST_NAMES_LENGTH, "Result: ");
+        print_test_res(sub_tests[0]);
         printf("Testing expansion:\n");
         printf("Before Size: %zu\n", my_arr.dynarr_size);
         printf("Before Cap.: %zu\n", my_arr.dynarr_capacity);
     }
     for(long unsigned int i = 0; i < my_arr.dynarr_size; ++i) {
         ptr_data = (ptr_head + i);
-        test_result &= (my_structs[i].anInt == ptr_data->anInt);
-        test_result &= (my_structs[i].aFloat == ptr_data->aFloat);
-        test_result &= (my_structs[i].aDouble == ptr_data->aDouble);
-        test_result &= (my_structs[i].aPointer == ptr_data->aPointer);
+        sub_tests[1] &= (my_structs[i].anInt == ptr_data->anInt);
+        sub_tests[1] &= (my_structs[i].aFloat == ptr_data->aFloat);
+        sub_tests[1] &= (my_structs[i].aDouble == ptr_data->aDouble);
+        sub_tests[1] &= (my_structs[i].aPointer == ptr_data->aPointer);
 
         if(!no_yapping) {
             printf("%ld-th Struct:\n", i);
@@ -156,17 +183,17 @@ bool test_struct(bool no_yapping) {
         }
     }
 
-    dynarr_push(&my_arr, &my_structs[2]);
+    ptr_head = (my_test_struct*)dynarr_push(&my_arr, &my_structs[2]);
     if(!no_yapping) {
         printf("After Size: %zu\n", my_arr.dynarr_size);
         printf("After Cap.: %zu\n", my_arr.dynarr_capacity);
     }
     for(long unsigned int i = 0; i < my_arr.dynarr_size; ++i) {
         ptr_data = (ptr_head + i);
-        test_result &= (my_structs[i].anInt == ptr_data->anInt);
-        test_result &= (my_structs[i].aFloat == ptr_data->aFloat);
-        test_result &= (my_structs[i].aDouble == ptr_data->aDouble);
-        test_result &= (my_structs[i].aPointer == ptr_data->aPointer);
+        sub_tests[1] &= (my_structs[i].anInt == ptr_data->anInt);
+        sub_tests[1] &= (my_structs[i].aFloat == ptr_data->aFloat);
+        sub_tests[1] &= (my_structs[i].aDouble == ptr_data->aDouble);
+        sub_tests[1] &= (my_structs[i].aPointer == ptr_data->aPointer);
 
         if(!no_yapping) {
             printf("%ld-th Struct:\n", i);
@@ -179,17 +206,19 @@ bool test_struct(bool no_yapping) {
         }
     }
 
-    dynarr_push(&my_arr, &my_structs[3]);
+    ptr_head = (my_test_struct*)dynarr_push(&my_arr, &my_structs[3]);
     if(!no_yapping) {
+        printf("%*s", TEST_NAMES_LENGTH, "Result: ");
+        print_test_res(sub_tests[1]);
         printf("End Size: %zu\n", my_arr.dynarr_size);
         printf("End Cap.: %zu\n\n", my_arr.dynarr_capacity);
     }
     for(long unsigned int i = 0; i < my_arr.dynarr_size; ++i) {
         ptr_data = (ptr_head + i);
-        test_result &= (my_structs[i].anInt == ptr_data->anInt);
-        test_result &= (my_structs[i].aFloat == ptr_data->aFloat);
-        test_result &= (my_structs[i].aDouble == ptr_data->aDouble);
-        test_result &= (my_structs[i].aPointer == ptr_data->aPointer);
+        sub_tests[2] &= (my_structs[i].anInt == ptr_data->anInt);
+        sub_tests[2] &= (my_structs[i].aFloat == ptr_data->aFloat);
+        sub_tests[2] &= (my_structs[i].aDouble == ptr_data->aDouble);
+        sub_tests[2] &= (my_structs[i].aPointer == ptr_data->aPointer);
 
         if(!no_yapping) {
             printf("%ld-th Struct:\n", i);
@@ -203,16 +232,24 @@ bool test_struct(bool no_yapping) {
     }
     dynarr_free(&my_arr);
 
+    if(!no_yapping) {
+        printf("%*s", TEST_NAMES_LENGTH, "Result: ");
+        print_test_res(sub_tests[2]);
+    }
+
+    for(int i = 0; i < n_sub; ++i) {
+        test_result &= sub_tests[i];
+    }
     return test_result;
 }
 
 int main() {
-    bool primitives_test = test_primitive_types(NO_YAPPING);
+    bool scalers_test = test_primitive_types(NO_YAPPING);
     bool struct_test = test_struct(NO_YAPPING);
 
     printf("Dynamic Array Test Suite Summary:\n");
     printf("%*s", TEST_NAMES_LENGTH, "Primitives Array Test: ");
-    if(primitives_test) {
+    if(scalers_test) {
         printf(GRN "S" RESET);
     } else {
         printf(RED "F" RESET);
