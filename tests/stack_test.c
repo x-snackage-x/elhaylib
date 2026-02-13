6799917fb13cefcab676d44fdda51d6939b5352e
Karim El-Hayawan <snackage@gmail.com>
2026-02-14 00:52:16 +0100

    stack test version with yapping possible

--- tests/stack_test.c
+++ tests/stack_test.c
@@ -15,7 +15,7 @@
 #define RESET "\x1B[0m"
 
 #define TEST_NAMES_LENGTH 25
-#define NO_YAPPING false
+#define NO_YAPPING true
 
 typedef enum { STRUCT_1 = 1000, STRUCT_2 = 1001 } my_struct_types;
 
@@ -109,7 +109,9 @@ bool test_primitive_types_stack(bool no_yapping) {
         sub_tests[2] &= int_data == my_res_2[index];
 
         if(!no_yapping)
-            printf("%d-th Popped Element = %d\n", index++, int_data);
+            printf("%d-th Popped Element = %d\n", index, int_data);
+
+        ++index;
 
         is_not_empty = stack_pop(my_stack, &int_data);
     }
@@ -137,8 +139,10 @@ bool test_struct_stack(bool no_yapping) {
     my_other_test_structure c_struct = {.gender = 'F', .name = "Jenny"};
     my_other_test_structure d_struct = {.gender = 'F', .name = "Nadezhda"};
 
-    if(!no_yapping)
+    if(!no_yapping) {
         printf("1. Testing stack with structs:\n");
+        printf("      Prints data as received from stack\n");
+    }
 
     // prep stack frames
     my_stack_frame frame1 = {.ptr_to_mystruct = &a_struct,
@@ -153,30 +157,82 @@ bool test_struct_stack(bool no_yapping) {
                                                     &frame1};
 
     // prep stack
-    stack_head* my_stack = stack_init(sizeof(my_stack_frame));
+    stack_head* my_stack = stack_init(sizeof(my_stack_frame*));
+
+    // this approach stores only the pointers to the frames on the stack
+    // to store the full frames inline:
+    // - change stack init size
+    // - pass &frame$i$ instead to the push function
+    // - change type of array stack_frame_ptrs to my_stack_frame
 
     // fill stack
-    stack_push(my_stack, &frame1);
-    stack_push(my_stack, &frame3);
-    stack_push(my_stack, &frame2);
-    stack_push(my_stack, &frame4);
+    my_stack_frame* frame_ptr1 = &frame1;
+    my_stack_frame* frame_ptr2 = &frame2;
+    my_stack_frame* frame_ptr3 = &frame3;
+    my_stack_frame* frame_ptr4 = &frame4;
+
+    stack_push(my_stack, &frame_ptr1);
+    stack_push(my_stack, &frame_ptr3);
+    stack_push(my_stack, &frame_ptr2);
+    stack_push(my_stack, &frame_ptr4);
 
     // pop all stack
-    my_stack_frame stack_frame_ptrs[4] = {0};
+    my_stack_frame* stack_frame_ptrs[4] = {0};
     int index = 0;
     bool is_not_empty = stack_pop(my_stack, &stack_frame_ptrs[index]);
     while(is_not_empty) {
-        sub_tests[0] &= stack_frame_ptrs[index].ptr_to_mystruct ==
+        sub_tests[0] &=
+            stack_frame_ptrs[index] == stack_frame_expected_ptrs[index];
+
+        sub_tests[0] &= stack_frame_ptrs[index]->ptr_to_mystruct ==
                         stack_frame_expected_ptrs[index]->ptr_to_mystruct;
 
-        sub_tests[0] &= stack_frame_ptrs[index].struct_type ==
+        sub_tests[0] &= stack_frame_ptrs[index]->struct_type ==
                         stack_frame_expected_ptrs[index]->struct_type;
         ++index;
         is_not_empty = stack_pop(my_stack, &stack_frame_ptrs[index]);
     }
 
+    // print end data
+    if(!no_yapping) {
+        my_struct_types buf_str_typ = 0;
+        my_test_structure buffer_a = {0};
+        my_other_test_structure buffer_b = {0};
+        int count = 1;
+        for(int i = 0; i < 4; ++i) {
+            buf_str_typ = stack_frame_ptrs[i]->struct_type;
+            printf("┌────────────────────────────┐\n");
+            switch(buf_str_typ) {
+                case STRUCT_1:
+                default:
+                    buffer_a = *(my_test_structure*)stack_frame_ptrs[i]
+                                    ->ptr_to_mystruct;
+                    printf("│ Index: %d  - Name: %-8s │ - %d\n", buffer_a.idx,
+                           buffer_a.name, count);
+                    break;
+                case STRUCT_2:
+                    buffer_b = *(my_other_test_structure*)stack_frame_ptrs[i]
+                                    ->ptr_to_mystruct;
+                    printf("│ Gender: %c - Name: %-8s │ - %d\n",
+                           buffer_b.gender, buffer_b.name, count);
+                    break;
+            }
+            printf("└────────────────────────────┘\n");
+            ++count;
+        }
+        printf("%*s", TEST_NAMES_LENGTH, "Result: ");
+        print_test_res(sub_tests[1]);
+    }
     // test stack is empty
-    sub_tests[1] &= !stack_pop(my_stack, &stack_frame_ptrs[index]);
+    bool pop_response = stack_pop(my_stack, &stack_frame_ptrs[index]);
+    sub_tests[1] &= !pop_response;
+
+    if(!no_yapping) {
+        printf("2. Testing stack is empty:\n");
+        printf("Stack is empty? %s\n", !pop_response ? "true" : "false");
+        printf("%*s", TEST_NAMES_LENGTH, "Result: ");
+        print_test_res(sub_tests[1]);
+    }
 
     stack_free(my_stack);
 
